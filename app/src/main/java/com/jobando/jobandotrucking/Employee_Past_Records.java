48ff9c45784ec31363f6cc05ffbe71475fd30488
Jasel <patriajasel@gmail.com>
2022-06-18 01:34:18 +0800

    4hours

--- app/src/main/java/com/jobando/jobandotrucking/Employee_Past_Records.java
+++ app/src/main/java/com/jobando/jobandotrucking/Employee_Past_Records.java
@@ -1,6 +1,7 @@
 package com.jobando.jobandotrucking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -15,6 +16,10 @@ public class Employee_Past_Records extends Employee_Navigation_Drawer_Base{
 
     ActivityEmployeePastRecordsBinding activityEmployeePastRecordsBinding;
 
+    String s1[], s2[], s3[];
+    int images[] = {R.drawable.place, R.drawable.place, R.drawable.place, R.drawable.place, R.drawable.place};
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,14 @@ public class Employee_Past_Records extends Employee_Navigation_Drawer_Base{
         setContentView(activityEmployeePastRecordsBinding.getRoot());
         allocateActivityTitle("Past Records");
 
+        recyclerView = findViewById(R.id.recyclerView);
 
+        s1 = getResources().getStringArray(R.array.sample_place);
+        s2 = getResources().getStringArray(R.array.description);
+        s3 = getResources().getStringArray(R.array.date);
+
+        Adapter_Past_Records myAdapter = new Adapter_Past_Records(this, s1, s2, s3, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
