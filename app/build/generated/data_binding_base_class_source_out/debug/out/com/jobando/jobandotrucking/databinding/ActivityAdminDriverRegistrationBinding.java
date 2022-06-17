bb42831abdf8003330aaa98fcbb259ad9d324928
Jasel <patriajasel@gmail.com>
2022-06-17 11:25:21 +0800

    Initial

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/jobando/jobandotrucking/databinding/ActivityAdminDriverRegistrationBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/jobando/jobandotrucking/databinding/ActivityAdminDriverRegistrationBinding.java
@@ -4,20 +4,101 @@ package com.jobando.jobandotrucking.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.jobando.jobandotrucking.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityAdminDriverRegistrationBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityAdminDriverRegistrationBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final TextInputEditText ageText;
+
+  @NonNull
+  public final TextInputEditText emailText;
+
+  @NonNull
+  public final TextInputEditText firstNameText;
+
+  @NonNull
+  public final TextInputEditText lastNameText;
+
+  @NonNull
+  public final TextInputEditText middleNameText;
+
+  @NonNull
+  public final TextInputEditText plateNumberText;
+
+  @NonNull
+  public final Button registerBtn;
+
+  @NonNull
+  public final TextInputLayout text1;
+
+  @NonNull
+  public final TextInputLayout text2;
+
+  @NonNull
+  public final TextInputLayout text3;
+
+  @NonNull
+  public final TextInputLayout text4;
+
+  @NonNull
+  public final TextInputLayout text5;
+
+  @NonNull
+  public final TextInputLayout text6;
+
+  @NonNull
+  public final TextInputLayout text7;
+
+  @NonNull
+  public final TextInputLayout text8;
+
+  @NonNull
+  public final TextInputEditText truckModelText;
+
+  @NonNull
+  public final TextInputEditText workExperienceText;
+
+  private ActivityAdminDriverRegistrationBinding(@NonNull ConstraintLayout rootView,
+      @NonNull TextInputEditText ageText, @NonNull TextInputEditText emailText,
+      @NonNull TextInputEditText firstNameText, @NonNull TextInputEditText lastNameText,
+      @NonNull TextInputEditText middleNameText, @NonNull TextInputEditText plateNumberText,
+      @NonNull Button registerBtn, @NonNull TextInputLayout text1, @NonNull TextInputLayout text2,
+      @NonNull TextInputLayout text3, @NonNull TextInputLayout text4,
+      @NonNull TextInputLayout text5, @NonNull TextInputLayout text6,
+      @NonNull TextInputLayout text7, @NonNull TextInputLayout text8,
+      @NonNull TextInputEditText truckModelText, @NonNull TextInputEditText workExperienceText) {
     this.rootView = rootView;
+    this.ageText = ageText;
+    this.emailText = emailText;
+    this.firstNameText = firstNameText;
+    this.lastNameText = lastNameText;
+    this.middleNameText = middleNameText;
+    this.plateNumberText = plateNumberText;
+    this.registerBtn = registerBtn;
+    this.text1 = text1;
+    this.text2 = text2;
+    this.text3 = text3;
+    this.text4 = text4;
+    this.text5 = text5;
+    this.text6 = text6;
+    this.text7 = text7;
+    this.text8 = text8;
+    this.truckModelText = truckModelText;
+    this.workExperienceText = workExperienceText;
   }
 
   @Override
@@ -43,10 +124,118 @@ public final class ActivityAdminDriverRegistrationBinding implements ViewBinding
 
   @NonNull
   public static ActivityAdminDriverRegistrationBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.ageText;
+      TextInputEditText ageText = ViewBindings.findChildViewById(rootView, id);
+      if (ageText == null) {
+        break missingId;
+      }
 
-    return new ActivityAdminDriverRegistrationBinding((ConstraintLayout) rootView);
+      id = R.id.emailText;
+      TextInputEditText emailText = ViewBindings.findChildViewById(rootView, id);
+      if (emailText == null) {
+        break missingId;
+      }
+
+      id = R.id.firstNameText;
+      TextInputEditText firstNameText = ViewBindings.findChildViewById(rootView, id);
+      if (firstNameText == null) {
+        break missingId;
+      }
+
+      id = R.id.lastNameText;
+      TextInputEditText lastNameText = ViewBindings.findChildViewById(rootView, id);
+      if (lastNameText == null) {
+        break missingId;
+      }
+
+      id = R.id.middleNameText;
+      TextInputEditText middleNameText = ViewBindings.findChildViewById(rootView, id);
+      if (middleNameText == null) {
+        break missingId;
+      }
+
+      id = R.id.plateNumberText;
+      TextInputEditText plateNumberText = ViewBindings.findChildViewById(rootView, id);
+      if (plateNumberText == null) {
+        break missingId;
+      }
+
+      id = R.id.registerBtn;
+      Button registerBtn = ViewBindings.findChildViewById(rootView, id);
+      if (registerBtn == null) {
+        break missingId;
+      }
+
+      id = R.id.text1;
+      TextInputLayout text1 = ViewBindings.findChildViewById(rootView, id);
+      if (text1 == null) {
+        break missingId;
+      }
+
+      id = com.karumi.dexter.R.id.text2;
+      TextInputLayout text2 = ViewBindings.findChildViewById(rootView, id);
+      if (text2 == null) {
+        break missingId;
+      }
+
+      id = R.id.text3;
+      TextInputLayout text3 = ViewBindings.findChildViewById(rootView, id);
+      if (text3 == null) {
+        break missingId;
+      }
+
+      id = R.id.text4;
+      TextInputLayout text4 = ViewBindings.findChildViewById(rootView, id);
+      if (text4 == null) {
+        break missingId;
+      }
+
+      id = R.id.text5;
+      TextInputLayout text5 = ViewBindings.findChildViewById(rootView, id);
+      if (text5 == null) {
+        break missingId;
+      }
+
+      id = R.id.text6;
+      TextInputLayout text6 = ViewBindings.findChildViewById(rootView, id);
+      if (text6 == null) {
+        break missingId;
+      }
+
+      id = R.id.text7;
+      TextInputLayout text7 = ViewBindings.findChildViewById(rootView, id);
+      if (text7 == null) {
+        break missingId;
+      }
+
+      id = R.id.text8;
+      TextInputLayout text8 = ViewBindings.findChildViewById(rootView, id);
+      if (text8 == null) {
+        break missingId;
+      }
+
+      id = R.id.truckModelText;
+      TextInputEditText truckModelText = ViewBindings.findChildViewById(rootView, id);
+      if (truckModelText == null) {
+        break missingId;
+      }
+
+      id = R.id.workExperienceText;
+      TextInputEditText workExperienceText = ViewBindings.findChildViewById(rootView, id);
+      if (workExperienceText == null) {
+        break missingId;
+      }
+
+      return new ActivityAdminDriverRegistrationBinding((ConstraintLayout) rootView, ageText,
+          emailText, firstNameText, lastNameText, middleNameText, plateNumberText, registerBtn,
+          text1, text2, text3, text4, text5, text6, text7, text8, truckModelText,
+          workExperienceText);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- app/src/main/java/com/jobando/jobandotrucking/Admin_Driver_Registration.java
+++ app/src/main/java/com/jobando/jobandotrucking/Admin_Driver_Registration.java
@@ -1,22 +1,195 @@
 package com.jobando.jobandotrucking;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jobando.jobandotrucking.databinding.ActivityAdminDashboardBinding;
 import com.jobando.jobandotrucking.databinding.ActivityAdminDriverRegistrationBinding;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 public class Admin_Driver_Registration extends Admin_Navigation_Drawer_Base{
 
     ActivityAdminDriverRegistrationBinding activityAdminDriverRegistrationBinding;
 
+    TextInputEditText fname, mname, lname, age, email, wexp, tmodel, platenum;
+    Button register;
+    Random random = new Random();
+
+    FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        db = FirebaseFirestore.getInstance();
+
         activityAdminDriverRegistrationBinding = ActivityAdminDriverRegistrationBinding.inflate(getLayoutInflater());
         setContentView(activityAdminDriverRegistrationBinding.getRoot());
         allocateActivityTitle("Driver Registration");
+
+        register = findViewById(R.id.registerBtn);
+        fname = findViewById(R.id.firstNameText);
+        mname = findViewById(R.id.middleNameText);
+        lname = findViewById(R.id.lastNameText);
+        age = findViewById(R.id.ageText);
+        email = findViewById(R.id.emailText);
+        wexp = findViewById(R.id.workExperienceText);
+        tmodel = findViewById(R.id.truckModelText);
+        platenum = findViewById(R.id.plateNumberText);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(fname.getText().toString().isEmpty() || mname.getText().toString().isEmpty() || lname.getText().toString().isEmpty() || age.getText().toString().isEmpty() || email.getText().toString().isEmpty() || wexp.getText().toString().isEmpty() ||tmodel.getText().toString().isEmpty() || platenum.getText().toString().isEmpty()){
+
+                    Toast.makeText(Admin_Driver_Registration.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    String fn = fname.getText().toString();
+                    String mn = mname.getText().toString();
+                    String ln = lname.getText().toString();
+                    String ag = age.getText().toString();
+                    String em = email.getText().toString();
+                    String we = wexp.getText().toString();
+                    String tm = tmodel.getText().toString();
+                    String pn = platenum.getText().toString();
+
+                    String uniqueId = UUID.randomUUID().toString();
+                    registerDriver(uniqueId,fn, mn, ln, ag, em, we, tm, pn);
+                    generateUsernamePassword(uniqueId, fn);
+                    clearFields();
+                }
+
+            }
+        });
+
+
+    }
+
+    private void registerDriver(String eID,String fName, String mName, String lName, String age, String eMail, String workExp, String tModel, String pNum) {
+
+
+        Map<String, Object> driverData = new HashMap<>();
+        driverData.put("EmployeeID", eID);
+        driverData.put("First Name", fName);
+        driverData.put("Middle Name", mName);
+        driverData.put("Last Name", lName);
+        driverData.put("Full Name", fName + " " + mName + " " + lName);
+        driverData.put("Age", age);
+        driverData.put("Email", eMail);
+        driverData.put("Work Experience", workExp);
+        driverData.put("Truck Model", tModel);
+        driverData.put("Plate Number", pNum);
+
+        db.collection("Drivers").document(eID)
+                .set(driverData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(Admin_Driver_Registration.this, "Driver Registration Success!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Admin_Driver_Registration.this, "Driver Registration Failed!", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+    }
+
+    private void generateUsernamePassword(String EmployeeID, String name){
+
+        String uname = name + random.nextInt(100);
+        String pass = generatePassword();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Username  & Password");
+        builder.setMessage("Username: " + uname + "\nPassword: " + pass);
+        builder.setPositiveButton("Copy to Clipboard", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                ClipboardManager cm =(ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("TextView", "Username: " + uname + "\nPassword: " + pass);
+                cm.setPrimaryClip(clip);
+
+                Toast.makeText(Admin_Driver_Registration.this, "Copied to Clipboard", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Map<String, Object> userData = new HashMap<>();
+                userData.put("EmployeeID", EmployeeID);
+                userData.put("username", uname);
+                userData.put("password", pass);
+                userData.put("EmployeeType", "Employee");
+
+                db.collection("Users").document(EmployeeID)
+                        .set(userData)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(Admin_Driver_Registration.this, "User Added Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Admin_Driver_Registration.this, "Adding User Failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void clearFields() {
+        fname.setText("");
+        mname.setText("");
+        lname.setText("");
+        age.setText("");
+        email.setText("");
+        wexp.setText("");
+        tmodel.setText("");
+        platenum.setText("");
+
+    }
+
+    private String generatePassword(){
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 8; i++){
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
